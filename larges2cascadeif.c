34fb5c7a4363ba5196142a64a7b5f207b43c02a2
Amritaa <amritaavarshini777@gmail.com>
2026-03-11 11:07:29 +0530

    Add program to find largest of two numbers using cascade if
    Implement program to find the largest of two numbers using cascade if statements.

---
+++ larges2cascadeif.c
@@ -0,0 +1,15 @@
+//program to find largest of 2 num using cascade if
+#include <stdio.h>
+void main(){
+    int a,b;
+    printf("Read a and b\n");
+    scanf("%d%d",&a,&b);
+    
+    if (a>b){
+        printf("a greater than b");
+    }
+    if (b>a){
+        printf("b greater than a");
+    }    
+}
+
