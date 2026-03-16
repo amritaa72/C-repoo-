a3f3c525c2e8749f97c80ac3b03925a470f2e670
Amritaa <amritaavarshini777@gmail.com>
2026-03-16 12:50:14 +0530

    Add program to check if a number is positive or negative

---
+++ posnevifelse.c
@@ -0,0 +1,18 @@
+//program to find +ve -ve
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("READ a");
+    scanf("%d",&a);
+    if(a<0)
+    {
+        printf("its -ve");
+    }
+    else
+    {
+        printf("its +ve");
+    }
+    return 0;
+}
