83aa9f6ebc1378b8bd65fb5ed31d6983549f7124
Amritaa <amritaavarshini777@gmail.com>
2026-03-16 12:57:10 +0530

    program to check if a number is even or odd

---
+++ evenoddifelse.c
@@ -0,0 +1,18 @@
+//program to find even odd
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("READ a");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("its even");
+    }
+    else
+    {
+        printf("its odd");
+    }
+    return 0;
+}
