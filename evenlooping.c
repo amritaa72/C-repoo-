d2ba44545b4363b99032cb9b5b621168008f717a
Amritaa <amritaavarshini777@gmail.com>
2026-03-19 15:48:14 +0530

    Refactor even number printing to use for loop
    Updated the loop structure to a for loop for printing even numbers.

---
+++ evenlooping.c
@@ -0,0 +1,28 @@
+//print even no
+#include <stdio.h>
+void main(){
+    /*
+    int i=0;
+    do
+    {
+        printf("%d\n",i);
+        i+=2;
+    }
+    while(i<51);
+    */
+    
+    /*
+    int i=0;
+    while(i<51)
+    {
+        printf("%d\n",i);
+        i+=2;
+    }
+    */
+    
+    for(int i=0;i<51;)
+    {
+        printf("%d\n",i);
+        i+=2;
+    }
+}
