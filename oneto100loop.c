59dbd5906664545d3d88e56695beb843c26a1665
Amritaa <amritaavarshini777@gmail.com>
2026-03-19 15:49:27 +0530

    Refactor loop structure
    Commented out the 'do' loop and added a 'while' loop structure, while , for to print 1 through 100

---
+++ oneto100loop.c
@@ -0,0 +1,24 @@
+//pgrm to print 1 through 10 in all loopin 
+#include <stdio.h>
+int main()
+{
+    int i=0;
+    do
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    while(i<11);
+    
+    /*while(i<11)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    */
+   /* for(int i=0;i<11;i++)
+    {
+        printf("%d\n",i);
+    }
+    */
+}
