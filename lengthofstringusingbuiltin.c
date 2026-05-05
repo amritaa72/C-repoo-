6ac47a24e75db6d6aaae47e9e023f41d89baaffd
Amritaa <amritaavarshini777@gmail.com>
2026-05-05 11:49:58 +0530

    program to calculate string length using strlen- builtin function

---
+++ lengthofstringusingbuiltin.c
@@ -0,0 +1,11 @@
+//using builtin- length of str
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[]="computer";
+    int len;
+    len=strlen(a);
+    printf("%d",len);
+    return 0;
+}
