ac7cf88b99f415d61e2b2dc3fee4353b34c15d54
Amritaa <amritaavarshini777@gmail.com>
2026-05-05 12:27:27 +0530

    string comparison program in compare2strbuiltin.c

---
+++ compare2strbuiltin.c
@@ -0,0 +1,10 @@
+//compare 2 strings
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char str1[20]="heblo";
+    char str2[30]="hello";
+    printf("The two strings are %d",strcmp(str1,str2));
+}
