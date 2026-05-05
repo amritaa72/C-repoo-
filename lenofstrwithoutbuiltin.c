237cbebb3661e016a526977870cc3401f0126037
Amritaa <amritaavarshini777@gmail.com>
2026-05-05 11:49:14 +0530

    program to calculate string length without builtin function
    Implement a program to calculate string length without using built-in functions.

---
+++ lenofstrwithoutbuiltin.c
@@ -0,0 +1,17 @@
+//without using builtin- length of str
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[]="computer";
+    //int len;
+    int i=0;
+    while(a[i]!='\0')
+    {
+        //len++;
+        i++;
+        
+    }
+    printf("%d",i);
+    return 0;
+}
