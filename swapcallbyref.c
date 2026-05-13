33e66d3111c8b0a72bff1bc3dc836e8893caff56
Amritaa <amritaavarshini777@gmail.com>
2026-05-13 10:59:25 +0530

    Add swap function to swap two integers
    Implement swap function using call by reference.

---
+++ swapcallbyref.c
@@ -0,0 +1,25 @@
+//call by ref
+#include <stdio.h>
+
+int swap( int * , int *);//passing 2 addresses of integers as a parameters //prototyping
+
+int swap(int *p, int *q)
+{
+    int temp;
+    temp=*p;
+    *p=*q;
+    *q=temp;
+}
+
+void main()
+{   
+    int a=5;
+    int b=10;
+    
+    
+
+    int sum;
+    sum=swap(&a,&b);
+    printf("a=%d\t b=%d",a,b);
+ 
+}
